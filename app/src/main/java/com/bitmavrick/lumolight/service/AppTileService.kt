1ed5bcbe088736c71afc0ab5596dbd5ae357ae5d
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-03-16 19:17:16 +0600

    Experiment with the tile

--- app/src/main/java/com/bitmavrick/lumolight/service/AppTileService.kt
+++ app/src/main/java/com/bitmavrick/lumolight/service/AppTileService.kt
@@ -1,7 +1,32 @@
 package com.bitmavrick.lumolight.service
 
 import android.service.quicksettings.TileService
+import android.util.Log
 
 class AppTileService : TileService() {
-    // TODO
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+        Log.e("Tile", " ------>> The tile added.")
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+        Log.e("Tile", " ------>> The tile is updating.")
+    }
+
+    override fun onStopListening() {
+        super.onStopListening()
+        Log.e("Tile", " ------>> The tile is now stop updating.")
+    }
+
+    override fun onClick() {
+        super.onClick()
+        Log.e("Tile", " ------>> Tile is clicked.")
+    }
+
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+        Log.e("Tile", " ------>> Tile is removed.")
+    }
 }
