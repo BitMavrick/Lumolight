29b5e1b1dc293d9eb6bad5e78bdb09c07395d80c
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-03-23 12:15:11 +0600

    Successfully maximize the display brightness

--- app/src/main/java/com/bitmavrick/lumolight/FlashActivity.kt
+++ app/src/main/java/com/bitmavrick/lumolight/FlashActivity.kt
@@ -1,9 +1,15 @@
 package com.bitmavrick.lumolight
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalContext
 import com.bitmavrick.lumolight.ui.theme.LumoLightTheme
 
 class FlashActivity: ComponentActivity() {
@@ -12,8 +18,27 @@ class FlashActivity: ComponentActivity() {
         
         setContent {
             LumoLightTheme {
+                UpdateBrightness()
                 Text(text = "Flash activity opened successfully")
             }
         }
     }
+}
+
+@Composable
+fun UpdateBrightness() {
+    val context = LocalContext.current
+    DisposableEffect(Unit) {
+        setBrightness(context, isFull = true)
+        onDispose {
+            setBrightness(context, isFull = false)
+        }
+    }
+}
+
+fun setBrightness(context: Context, isFull: Boolean) {
+    val activity = context as? Activity ?: return
+    val layoutParams: WindowManager.LayoutParams = activity.window.attributes
+    layoutParams.screenBrightness = if (isFull) 1f else WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE
+    activity.window.attributes = layoutParams
 }
