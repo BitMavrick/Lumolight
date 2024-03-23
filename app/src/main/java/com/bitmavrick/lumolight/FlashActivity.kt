2256666debcd9ef8450ecec35bc4b258b41c80ee
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-03-23 12:25:11 +0600

    Successfully keep screen wake during the flash light on

--- app/src/main/java/com/bitmavrick/lumolight/FlashActivity.kt
+++ app/src/main/java/com/bitmavrick/lumolight/FlashActivity.kt
@@ -2,6 +2,7 @@ package com.bitmavrick.lumolight
 
 import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
@@ -19,12 +20,15 @@ class FlashActivity: ComponentActivity() {
         setContent {
             LumoLightTheme {
                 UpdateBrightness()
+                KeepScreenOn()
                 Text(text = "Flash activity opened successfully")
             }
         }
     }
 }
 
+
+// Code for the max Brightness
 @Composable
 fun UpdateBrightness() {
     val context = LocalContext.current
@@ -41,4 +45,27 @@ fun setBrightness(context: Context, isFull: Boolean) {
     val layoutParams: WindowManager.LayoutParams = activity.window.attributes
     layoutParams.screenBrightness = if (isFull) 1f else WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE
     activity.window.attributes = layoutParams
+}
+
+
+// Code for the keep screen wake
+@Composable
+fun KeepScreenOn() {
+    val context = LocalContext.current
+    DisposableEffect(Unit) {
+        val window = context.findActivity()?.window
+        window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        onDispose {
+            window?.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
+    }
+}
+
+fun Context.findActivity(): Activity? {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is Activity) return context
+        context = context.baseContext
+    }
+    return null
 }
