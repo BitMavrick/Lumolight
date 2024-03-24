c15a18ca738ecc36af70980ad8ee13eecc318b1e
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-03-24 23:01:58 +0600

    Created a new screen for flash

--- app/src/main/java/com/bitmavrick/lumolight/FlashActivity.kt
+++ app/src/main/java/com/bitmavrick/lumolight/FlashActivity.kt
@@ -7,10 +7,16 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import com.bitmavrick.lumolight.ui.theme.LumoLightTheme
 
 class FlashActivity: ComponentActivity() {
@@ -21,9 +27,22 @@ class FlashActivity: ComponentActivity() {
             LumoLightTheme {
                 UpdateBrightness()
                 KeepScreenOn()
-                Text(text = "Flash is turned on")
+                FlashScreen()
             }
         }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable()
+fun FlashScreen(){
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Flash on")
     }
 }
 
