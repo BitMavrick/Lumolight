760112dcd73f3e663646bb37a94892d1f2ada723
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-04-02 16:22:37 +0600

    Clean install

--- app/src/main/java/com/bitmavrick/lumolight/MainActivity.kt
+++ app/src/main/java/com/bitmavrick/lumolight/MainActivity.kt
@@ -1,64 +1,46 @@
 package com.bitmavrick.lumolight
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.bitmavrick.lumolight.ui.theme.LumoLightTheme
+import com.bitmavrick.lumolight.ui.theme.LumolightTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LumoLightTheme {
+            LumolightTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen(
-                        onClickButton = {
-                            Intent(applicationContext, FlashActivity::class.java).also {
-                                startActivity(it) // Should be navigation
-                            }
-                        }
-                    )
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
-fun MainScreen(
-    onClickButton: () -> Unit = {}
-) {
-    Column(
-        Modifier.fillMaxSize(),
-        Arrangement.Bottom,
-        Alignment.CenterHorizontally
-    ) {
-        Button(
-            onClick = {
-                onClickButton()
-            },
-            modifier = Modifier.padding(bottom = 80.dp)
-        ) {
-            Text(text = "Turn On")
-        }
+fun GreetingPreview() {
+    LumolightTheme {
+        Greeting("Android")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,12 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
@@ -13,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Lumo Light"
+rootProject.name = "Lumolight"
 include(":app")
