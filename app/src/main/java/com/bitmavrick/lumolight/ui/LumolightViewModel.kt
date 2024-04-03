6c9b98897d92e7601d41d23d94e51a1adbaa9b28
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-04-03 19:26:55 +0600

    Successfully setting up the navigation bar

--- app/src/main/java/com/bitmavrick/lumolight/ui/LumolightViewModel.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/LumolightViewModel.kt
@@ -1,5 +1,6 @@
 package com.bitmavrick.lumolight.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.bitmavrick.lumolight.ui.utils.NavigationItem
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,6 +13,7 @@ class LumolightViewModel : ViewModel() {
     val uiState: StateFlow<LumolightUiState> = _uiState
 
     fun updateCurrentNavigationItem(navigationItem: NavigationItem){
+        Log.d("Tab", "The value is updating")
         _uiState.update {
             it.copy(
                 currentNavigationItem = navigationItem
--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/HomeScreen.kt
@@ -1,10 +1,23 @@
 package com.bitmavrick.lumolight.ui.screen
 
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.PermanentNavigationDrawer
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.bitmavrick.lumolight.ui.LumolightUiState
 import com.bitmavrick.lumolight.ui.utils.LumolightNavigationType
@@ -33,6 +46,59 @@ fun HomeScreen(
             text = "Flashlight"
         )
     )
+
+    when(navigationType){
+        LumolightNavigationType.BOTTOM_NAVIGATION -> {
+            Column(
+                Modifier.fillMaxSize()
+            ) {
+                Box(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(text = "This is the bottom navigation bar")
+                }
+                LumolightBottomNavigationBar(
+                    currentTab = uiState.currentNavigationItem,
+                    navigationItemContentList = navigationItemContentList,
+                    onTabPressed =  onTabPressed
+                )
+            }
+        }
+
+        LumolightNavigationType.NAVIGATION_RAIL -> {
+            Column(
+                Modifier.fillMaxSize()
+            ) {
+                Box(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(text = "This is the navigation rail")
+                }
+                LumolightNavigationRail(
+                    currentTab = uiState.currentNavigationItem,
+                    navigationItemContentList = navigationItemContentList,
+                    onTabPressed = onTabPressed
+                )
+            }
+        }
+
+        LumolightNavigationType.PERMANENT_NAVIGATION_DRAWER -> {
+            Column(
+                Modifier.fillMaxSize()
+            ) {
+                Box(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(text = "This is the navigation rail")
+                }
+                LumolightNavigationRail(
+                    currentTab = uiState.currentNavigationItem,
+                    navigationItemContentList = navigationItemContentList,
+                    onTabPressed = onTabPressed
+                )
+            }
+        }
+    }
 }
 
 @Composable
@@ -40,9 +106,24 @@ private fun LumolightBottomNavigationBar(
     currentTab: NavigationItem,
     onTabPressed: ((NavigationItem) -> Unit),
     navigationItemContentList: List<NavigationItemContent>,
+    modifier: Modifier = Modifier
 ){
-    // Bottom Navigation Bar
-
+    NavigationBar(modifier = modifier) {
+        for (navItem in navigationItemContentList){
+            NavigationBarItem(
+                selected = currentTab == navItem.screenType,
+                onClick = { onTabPressed(navItem.screenType) },
+                icon = {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.text
+                    )
+                },
+                label = { Text(text = navItem.text) },
+                alwaysShowLabel = false
+            )
+        }
+    }
 }
 
 @Composable
@@ -50,20 +131,39 @@ private fun LumolightNavigationRail(
     currentTab: NavigationItem,
     onTabPressed: ((NavigationItem) -> Unit),
     navigationItemContentList: List<NavigationItemContent>,
+    modifier: Modifier = Modifier
 ){
-    // Navigation Rail
+    NavigationRail(modifier = modifier) {
+        Spacer(Modifier.weight(1f))
+        for (navItem in navigationItemContentList){
+            NavigationRailItem(
+                selected = currentTab == navItem.screenType,
+                onClick = { onTabPressed(navItem.screenType) },
+                icon = {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.text
+                    )
+                },
+                label = { Text(text = navItem.text) },
+                alwaysShowLabel = false
+            )
+        }
+    }
 
 }
 
+/*
 @Composable
 private fun LumolightNavigationDrawer(
     currentTab: NavigationItem,
     onTabPressed: ((NavigationItem) -> Unit),
     navigationItemContentList: List<NavigationItemContent>,
 ){
-    // Navigation Drawer
+
 
 }
+*/
 
 private data class NavigationItemContent(
     val screenType: NavigationItem,
