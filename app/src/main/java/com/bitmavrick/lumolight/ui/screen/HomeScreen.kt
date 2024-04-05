b64bd6cf004ace9a2fd0295c2faf56e4db7ae82c
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-04-05 20:43:16 +0600

    Optimizing

--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.bitmavrick.lumolight.ui.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,15 +14,11 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.bitmavrick.lumolight.ui.LumolightUiState
 import com.bitmavrick.lumolight.ui.utils.LumolightNavigationType
