af8dccd5a1de4350ff07b633248d51944dd04ba2
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-04-17 13:24:02 +0600

    Setting up the basic structure

---
+++ app/src/main/java/com/bitmavrick/lumolight/ui/component/BannerAds.kt
@@ -0,0 +1,19 @@
+package com.bitmavrick.lumolight.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+import com.bitmavrick.lumolight.util.Ads
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
+@Composable
+fun BannerAd(){
+    AndroidView(factory = {context ->
+        AdView(context).apply {
+            setAdSize(AdSize.BANNER)
+            adUnitId = Ads.GoogleAds.ID
+            loadAd(AdRequest.Builder().build())
+        }
+    })
+}
---
+++ app/src/main/java/com/bitmavrick/lumolight/util/Ads.kt
@@ -0,0 +1,7 @@
+package com.bitmavrick.lumolight.util
+
+class Ads {
+    object GoogleAds{
+        const val ID: String = "ca-app-pub-3940256099942544/9214589741" // Testing
+    }
+}
