1e39168a7fce623702d54cc66c89a9d030b5b32d
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-04-17 18:33:22 +0600

    Handling system bars color correctly

--- app/src/main/java/com/bitmavrick/lumolight/activity/MainActivity.kt
+++ app/src/main/java/com/bitmavrick/lumolight/activity/MainActivity.kt
@@ -2,19 +2,31 @@ package com.bitmavrick.lumolight.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import android.graphics.Color
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
 import com.bitmavrick.lumolight.ui.theme.LumolightTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
         setContent {
             LumolightTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,9 +34,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text(text = "Lumolight")
+                    LumolightTheme {
+                        MainScreen()
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold {
+        Column(
+            Modifier
+                .padding(it)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Lumolight")
         }
     }
 }
