6b4a097e59b10263c18d98c2738350f5f0f5dd9b
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-04-17 22:17:57 +0600

    Setting up the preview for each screen

--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/home/HomeScreen.kt
@@ -12,9 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.bitmavrick.lumolight.activity.core.Screen
+import com.bitmavrick.lumolight.ui.theme.LumolightTheme
 
 @Composable
 fun HomeScreen(
@@ -36,5 +39,13 @@ fun HomeScreen(
                 Text(text = "Settings")
             }
         }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HomeScreenPreview() {
+    LumolightTheme {
+        HomeScreen(navController = rememberNavController())
     }
 }
--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/setting/SettingScreen.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/setting/SettingScreen.kt
@@ -12,9 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.bitmavrick.lumolight.activity.core.Screen
+import com.bitmavrick.lumolight.ui.theme.LumolightTheme
 
 @Composable
 fun SettingScreen(
@@ -36,5 +39,13 @@ fun SettingScreen(
                 Text(text = "Home")
             }
         }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SettingScreenPreview() {
+    LumolightTheme {
+        SettingScreen(navController = rememberNavController())
     }
 }
