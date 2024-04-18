f577f7f6e62807c1ad9d14d0ab5636a0fe7c11ab
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-04-18 18:43:11 +0600

    Making the dropdown menu in the home screen

--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/home/HomeScreen.kt
@@ -1,25 +1,39 @@
 package com.bitmavrick.lumolight.ui.screen.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.StarRate
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,8 +49,9 @@ fun HomeScreen(
     Scaffold (
         topBar = {
             HomeScreenTopBar(
-                onClickSettings = { navController.navigate(Screen.SettingScreen.route) },
-                onClickMore = {}
+                onClickSettings = {
+                    navController.navigate(Screen.SettingScreen.route)
+                }
             )
         },
         content = { innerPadding ->
@@ -63,8 +78,9 @@ fun HomeScreen(
 @Composable
 fun HomeScreenTopBar(
     onClickSettings: () -> Unit,
-    onClickMore: () -> Unit
 ) {
+    var expanded by remember { mutableStateOf(false) }
+
     TopAppBar(
         title = {
             Text(
@@ -85,11 +101,27 @@ fun HomeScreenTopBar(
             }
 
             IconButton(
-                onClick = { onClickMore() }
+                onClick = { expanded = true }
             ) {
                 Icon(
                     imageVector = Icons.Outlined.MoreVert,
                     contentDescription = "Localized description"
+                )
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Rate Us")
+                    },
+                    onClick = { /*TODO*/ },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.StarRate,
+                            contentDescription = null)
+                    }
                 )
             }
         }
