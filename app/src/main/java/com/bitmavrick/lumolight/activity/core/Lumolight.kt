967a7607cbcbaad07e9d43eebe6b36d761099c7f
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-04-18 18:06:23 +0600

    Remove the custom navigation animation for now

--- app/src/main/java/com/bitmavrick/lumolight/activity/core/Lumolight.kt
+++ app/src/main/java/com/bitmavrick/lumolight/activity/core/Lumolight.kt
@@ -1,9 +1,5 @@
 package com.bitmavrick.lumolight.activity.core
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -14,34 +10,10 @@ import com.bitmavrick.lumolight.ui.screen.setting.SettingScreen
 @Composable
 fun Lumolight() {
     val navController = rememberNavController()
-    val enterAnimationDuration = 500
-    val exitAnimationDuration = 450
     NavHost(
         navController = navController,
-        startDestination = Screen.HomeScreen.route,
-        enterTransition = {
-            fadeIn(animationSpec = tween(enterAnimationDuration)) + slideIntoContainer(
-                AnimatedContentTransitionScope.SlideDirection.Up, tween(enterAnimationDuration)
-            )
-        },
-        exitTransition =  {
-            fadeOut(animationSpec = tween(exitAnimationDuration)) + slideOutOfContainer(
-                AnimatedContentTransitionScope.SlideDirection.Up, tween(exitAnimationDuration)
-            )
-        },
-
-        popEnterTransition = {
-            fadeIn(animationSpec = tween(enterAnimationDuration)) + slideIntoContainer(
-                AnimatedContentTransitionScope.SlideDirection.Down, tween(enterAnimationDuration)
-            )
-        },
-
-        popExitTransition = {
-            fadeOut(animationSpec = tween(exitAnimationDuration)) + slideOutOfContainer(
-                AnimatedContentTransitionScope.SlideDirection.Down, tween(exitAnimationDuration)
-            )
-        }
-
+        startDestination = Screen.HomeScreen.route
+        // DRAFT : LUMOLIGHT CUSTOM NAVIGATION ANIMATION
     ) {
         composable(route = Screen.HomeScreen.route){
             HomeScreen(
--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/setting/SettingScreen.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/setting/SettingScreen.kt
@@ -1,29 +1,21 @@
 package com.bitmavrick.lumolight.ui.screen.setting
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBackIosNew
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
@@ -48,21 +40,19 @@ fun SettingScreen(
             )
         },
         content = { innerPadding ->
-            Column(
-                Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+            LazyColumn(
+                contentPadding = innerPadding,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                Text(text = "Lumolight Setting Screen")
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(
-                    onClick = {
-                        navController.popBackStack()
-                    }
-                ) {
-                    Text(text = "Go Home")
+                val list = (0..100).map { it.toString() }
+                items(count = list.size) {
+                    Text(
+                        text = list[it],
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    )
                 }
             }
         }
