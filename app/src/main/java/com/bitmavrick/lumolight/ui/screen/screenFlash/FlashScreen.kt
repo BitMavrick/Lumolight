ce532ad3326181122c8edb08c65883032ac7b219
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-05-05 15:38:46 +0600

    Successfully shows the timer

--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/screenFlash/FlashScreen.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/screenFlash/FlashScreen.kt
@@ -1,15 +1,25 @@
 package com.bitmavrick.lumolight.ui.screen.screenFlash
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.toColorInt
@@ -30,9 +40,17 @@ fun FlashScreen(
     SetBrightness(screenFlashUiState.screenFlashBrightnessValue)
     KeepScreenOn()
 
+    var time by remember { mutableIntStateOf(screenFlashUiState.screenFlashDurationMin) }
+
     if(screenFlashUiState.screenFlashDurationMin != -1){
+
+        time = screenFlashUiState.screenFlashDurationMin * 60
+
         LaunchedEffect(key1 = Unit) {
-            delay(screenFlashUiState.screenFlashDurationMin * 60 * 1000L)
+            for (i in 1..screenFlashUiState.screenFlashDurationMin * 60) {
+                delay(1000L)
+                time--
+            }
             navController.popBackStack()
         }
     }
@@ -50,6 +68,18 @@ fun FlashScreen(
                     .padding(16.dp),
                 verticalArrangement = Arrangement.Bottom,
             ){
+                Column(
+                    Modifier.weight(1f).fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    if(time != -1){
+                        Text(
+                            text = formatTime(time),
+                            color = MaterialTheme.colorScheme.primary,
+                        )
+                    }
+                }
                 CustomOutlinedButton(
                     buttonText = "Close",
                     onClick = {
@@ -59,6 +89,13 @@ fun FlashScreen(
             }
         }
     )
+}
+
+@SuppressLint("DefaultLocale")
+fun formatTime(seconds: Int): String {
+    val minutes = seconds / 60
+    val remainingSeconds = seconds % 60
+    return String.format("%02d:%02d", minutes, remainingSeconds)
 }
 
 @Preview(showBackground = true, showSystemUi = true)
