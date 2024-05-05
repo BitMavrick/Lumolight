963fbc42875160a81575140a531b3ab390e5810d
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-05-05 15:40:11 +0600

    Optimizing code

--- app/src/main/java/com/bitmavrick/lumolight/ui/tab/quickAction/utils.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/tab/quickAction/utils.kt
@@ -1,7 +1,6 @@
 package com.bitmavrick.lumolight.ui.tab.quickAction
 
 import android.content.Context
-import android.widget.Toast
 import androidx.navigation.NavController
 import com.bitmavrick.lumolight.activity.core.Screen
 
@@ -12,36 +11,24 @@ fun startButtonActionHandler(
     context: Context
 ){
     if (uiState.segmentedButtonSelectedIndex == 0 && !uiState.startButtonStatus){
-
         navController.navigate(Screen.FlashScreen.route)
-
-        Toast.makeText(context, "Screen flash is on", Toast.LENGTH_SHORT).show()
         viewModel.activeStartButton()
     }
 
     if (uiState.segmentedButtonSelectedIndex == 1 && !uiState.startButtonStatus){
-
         viewModel.toggleFlashLight(context, true)
         navController.navigate(Screen.FlashScreen.route)
-
-        Toast.makeText(context, "Both flash is on", Toast.LENGTH_SHORT).show()
         viewModel.activeStartButton()
     }
 
 
     if (uiState.segmentedButtonSelectedIndex == 2 && !uiState.startButtonStatus){
-
         viewModel.toggleFlashLight(context, true)
-
-        // Toast.makeText(context, "Back flash is on", Toast.LENGTH_SHORT).show()
         viewModel.activeStartButton()
     }
 
     if (uiState.segmentedButtonSelectedIndex == 2 && uiState.startButtonStatus){
-
         viewModel.toggleFlashLight(context, false)
-
-        // Toast.makeText(context, "Back flash is off", Toast.LENGTH_SHORT).show()
         viewModel.stopStartButton()
     }
 }
