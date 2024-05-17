a8889d58902d7ba8ae15ecb0e6222e3ad4d4adc9
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-05-17 22:07:22 +0600

    Adding tile services

---
+++ app/src/main/java/com/bitmavrick/lumolight/activity/FlashTileActivity.kt
@@ -0,0 +1,15 @@
+package com.bitmavrick.lumolight.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class FlashTileActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
