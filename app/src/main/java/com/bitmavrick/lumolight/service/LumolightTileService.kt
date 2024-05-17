3707e68c4100fc7c7683a0da53bf3006eebfc972
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-05-17 22:40:35 +0600

    Done for today

--- app/src/main/java/com/bitmavrick/lumolight/service/LumolightTileService.kt
+++ app/src/main/java/com/bitmavrick/lumolight/service/LumolightTileService.kt
@@ -2,42 +2,28 @@ package com.bitmavrick.lumolight.service
 
 import android.app.PendingIntent
 import android.content.Intent
-import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import androidx.core.service.quicksettings.PendingIntentActivityWrapper
 import androidx.core.service.quicksettings.TileServiceCompat
 import com.bitmavrick.lumolight.activity.FlashTileActivity
 
 class LumolightTileService : TileService() {
-    private var active = false
 
     override fun onClick() {
         super.onClick()
 
-        qsTile.state = if(!active) {
-            Tile.STATE_ACTIVE
-        }else {
-            Tile.STATE_INACTIVE
-        }
-        active = !active
-        qsTile.label = if(active){ "Flash On" }else{ "Lumolight" }
-
-        qsTile.updateTile()
-
-        if(active){
-            Intent(applicationContext, FlashTileActivity::class.java).also {
-                it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                TileServiceCompat.startActivityAndCollapse(
+        Intent(applicationContext, FlashTileActivity::class.java).also {
+            it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            TileServiceCompat.startActivityAndCollapse(
+                this@LumolightTileService,
+                PendingIntentActivityWrapper(
                     this@LumolightTileService,
-                    PendingIntentActivityWrapper(
-                        this@LumolightTileService,
-                        0,
-                        it,
-                        PendingIntent.FLAG_UPDATE_CURRENT,
-                        true
-                    )
+                    0,
+                    it,
+                    PendingIntent.FLAG_UPDATE_CURRENT,
+                    true
                 )
-            }
+            )
         }
     }
 }
