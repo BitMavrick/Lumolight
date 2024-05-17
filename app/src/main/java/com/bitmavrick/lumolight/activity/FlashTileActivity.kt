c6f1120e07c4b26c201410b9b51708e3e280d680
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-05-17 22:20:05 +0600

    Saving the progress

--- app/src/main/java/com/bitmavrick/lumolight/activity/FlashTileActivity.kt
+++ app/src/main/java/com/bitmavrick/lumolight/activity/FlashTileActivity.kt
@@ -3,13 +3,33 @@ package com.bitmavrick.lumolight.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 class FlashTileActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-
+            TileFlash()
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TileFlash() {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello World")
     }
 }
