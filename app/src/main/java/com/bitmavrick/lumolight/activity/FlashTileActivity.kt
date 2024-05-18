6b6292bd69734ba77296ea19c3724d9dc9a32355
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-05-18 07:39:15 +0600

    Saving the progress

--- app/src/main/java/com/bitmavrick/lumolight/activity/FlashTileActivity.kt
+++ app/src/main/java/com/bitmavrick/lumolight/activity/FlashTileActivity.kt
@@ -1,20 +1,34 @@
 package com.bitmavrick.lumolight.activity
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bitmavrick.lumolight.system.KeepScreenOn
+import com.bitmavrick.lumolight.ui.tab.CustomOutlinedButton
+import kotlin.system.exitProcess
 
 class FlashTileActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
 
         setContent {
             TileFlash()
@@ -25,11 +39,23 @@ class FlashTileActivity: ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun TileFlash() {
-    Column(
-        Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Hello World")
-    }
+    KeepScreenOn()
+    Scaffold (
+        content = { innerPadding ->
+            Column(
+                Modifier
+                    .fillMaxSize().padding(innerPadding)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CustomOutlinedButton(
+                    buttonText = "Exit",
+                    onClick = {
+                        exitProcess(0)
+                    }
+                )
+            }
+        }
+    )
 }
