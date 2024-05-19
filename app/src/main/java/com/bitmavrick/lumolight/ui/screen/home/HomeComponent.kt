b6b968a684f10802fc284a84cb27ed6edcd5e855
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-05-19 10:33:30 +0600

    Saving the progress

--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/home/HomeComponent.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/home/HomeComponent.kt
@@ -1,15 +1,168 @@
 package com.bitmavrick.lumolight.ui.screen.home
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Comment
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.PrivacyTip
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material.icons.outlined.Sos
+import androidx.compose.material.icons.outlined.Spoke
+import androidx.compose.material.icons.outlined.StarRate
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TabPosition
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenTopBar(
+    onClickSettings: () -> Unit,
+    onClickAbout : () -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    TopAppBar(
+        title = {
+            Text(
+                text = "Lumolight",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        actions = {
+
+
+            IconButton(
+                onClick = { /* TODO */ }
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Sos,
+                    contentDescription = null
+                )
+            }
+
+
+            IconButton(
+                onClick = { onClickSettings() }
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Settings,
+                    contentDescription = null
+                )
+            }
+
+            IconButton(
+                onClick = { expanded = true }
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.MoreVert,
+                    contentDescription = null
+                )
+            }
+
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "About")
+                    },
+                    onClick = { onClickAbout() },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.Info,
+                            contentDescription = null)
+                    }
+                )
+
+
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Feedback")
+                    },
+                    onClick = { /*TODO*/ },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.AutoMirrored.Outlined.Comment,
+                            contentDescription = null)
+                    }
+                )
+
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Share")
+                    },
+                    onClick = { /*TODO*/ },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.Share,
+                            contentDescription = null)
+                    }
+                )
+
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Privacy Policy")
+                    },
+                    onClick = { /*TODO*/ },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.PrivacyTip,
+                            contentDescription = null)
+                    }
+                )
+
+                HorizontalDivider()
+
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Rate Lumolight")
+                    },
+                    onClick = { /*TODO*/ },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.StarRate,
+                            contentDescription = null)
+                    }
+                )
+
+                DropdownMenuItem(
+                    text = {
+                        Text(text = "Try more apps")
+                    },
+                    onClick = { /*TODO*/ },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.Spoke,
+                            contentDescription = null)
+                    }
+                )
+
+
+            }
+        }
+    )
+}
 
 @Composable
 fun CustomTabRow(
--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/home/HomeScreen.kt
@@ -10,27 +10,14 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.Comment
 import androidx.compose.material.icons.filled.CameraRear
 import androidx.compose.material.icons.filled.DynamicForm
 import androidx.compose.material.icons.filled.PhoneIphone
 import androidx.compose.material.icons.outlined.CameraRear
 import androidx.compose.material.icons.outlined.DynamicForm
-import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material.icons.outlined.MoreVert
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material.icons.outlined.Smartphone
-import androidx.compose.material.icons.outlined.Sos
-import androidx.compose.material.icons.outlined.Spoke
-import androidx.compose.material.icons.outlined.StarRate
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LeadingIconTab
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -39,20 +26,14 @@ import androidx.compose.material3.TabPosition
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.lerp
@@ -215,134 +196,6 @@ fun HomeScreen(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun HomeScreenTopBar(
-    onClickSettings: () -> Unit,
-    onClickAbout : () -> Unit
-) {
-    var expanded by remember { mutableStateOf(false) }
-
-    TopAppBar(
-        title = {
-            Text(
-                text = "Lumolight",
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                fontWeight = FontWeight.Bold
-            )
-        },
-        actions = {
-
-
-            IconButton(
-                onClick = { /* TODO */ }
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.Sos,
-                    contentDescription = null
-                )
-            }
-
-
-            IconButton(
-                onClick = { onClickSettings() }
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.Settings,
-                    contentDescription = null
-                )
-            }
-
-            IconButton(
-                onClick = { expanded = true }
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.MoreVert,
-                    contentDescription = null
-                )
-            }
-
-
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false }
-            ) {
-
-                DropdownMenuItem(
-                    text = {
-                        Text(text = "About")
-                    },
-                    onClick = { onClickAbout() },
-                    leadingIcon = {
-                        Icon(imageVector = Icons.Outlined.Info,
-                            contentDescription = null)
-                    }
-                )
-
-
-                DropdownMenuItem(
-                    text = {
-                        Text(text = "Feedback")
-                    },
-                    onClick = { /*TODO*/ },
-                    leadingIcon = {
-                        Icon(imageVector = Icons.AutoMirrored.Outlined.Comment,
-                            contentDescription = null)
-                    }
-                )
-
-                DropdownMenuItem(
-                    text = {
-                        Text(text = "Share")
-                    },
-                    onClick = { /*TODO*/ },
-                    leadingIcon = {
-                        Icon(imageVector = Icons.Outlined.Share,
-                            contentDescription = null)
-                    }
-                )
-
-                DropdownMenuItem(
-                    text = {
-                        Text(text = "About us")
-                    },
-                    onClick = { /*TODO*/ },
-                    leadingIcon = {
-                        Icon(imageVector = Icons.Outlined.Info,
-                            contentDescription = null)
-                    }
-                )
-
-                HorizontalDivider()
-
-                DropdownMenuItem(
-                    text = {
-                        Text(text = "Rate Lumolight")
-                    },
-                    onClick = { /*TODO*/ },
-                    leadingIcon = {
-                        Icon(imageVector = Icons.Outlined.StarRate,
-                            contentDescription = null)
-                    }
-                )
-
-                DropdownMenuItem(
-                    text = {
-                        Text(text = "Try more apps")
-                    },
-                    onClick = { /*TODO*/ },
-                    leadingIcon = {
-                        Icon(imageVector = Icons.Outlined.Spoke,
-                            contentDescription = null)
-                    }
-                )
-
-
-            }
-        }
-    )
-}
 
 data class TabItem(
     val title: String,
