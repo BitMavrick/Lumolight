3fb8135995dd39785d164573ead34a13ef76eb13
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-05-31 06:24:54 +0600

    Update the sos screen design

--- app/src/main/java/com/bitmavrick/lumolight/ui/screen/sos/SosScreen.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/screen/sos/SosScreen.kt
@@ -1,16 +1,24 @@
 package com.bitmavrick.lumolight.ui.screen.sos
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bitmavrick.lumolight.system.KeepScreenOn
+import com.bitmavrick.lumolight.system.SetBrightness
+import com.bitmavrick.lumolight.ui.tab.CustomOutlinedButton
 
 @Composable
 fun SosScreen(
@@ -19,21 +27,40 @@ fun SosScreen(
 ) {
     val context = LocalContext.current
 
-    Column(
-        Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(
-            onClick = {
-                sosViewModel.toggleFlashLight(context, false)
-                sosViewModel.updateFlashlightStatus(false)
-                onExitClick()
+    SetBrightness(1f)
+    KeepScreenOn()
+
+    Scaffold (
+        content = { innerPadding ->
+            Column (
+                Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = Color.Red
+                    )
+                    .padding(innerPadding)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Bottom,
+            ){
+                Column(
+                    Modifier.weight(1f).fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                }
+                CustomOutlinedButton(
+                    buttonText = "Exit",
+                    onClick = {
+                        sosViewModel.toggleFlashLight(context, false)
+                        sosViewModel.updateFlashlightStatus(false)
+                        onExitClick()
+                    },
+                    color = MaterialTheme.colorScheme.primary
+                )
             }
-        ) {
-            Text(text = "Exit SOS Screen")
         }
-    }
+    )
 }
 
 @Preview(showBackground = true)
