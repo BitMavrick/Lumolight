5b9d572a162dc5ba77931b7b9850ded8b258d55b
bitmavrick <email.mehedi.bd@gmail.com>
2025-06-17 08:49:31 +0600

    Update the changes

--- app/src/main/java/com/bitmavrick/lumolight/ui/theme/Theme.kt
+++ app/src/main/java/com/bitmavrick/lumolight/ui/theme/Theme.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -168,7 +167,6 @@ fun LumolightTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
