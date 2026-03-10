6e9b63f6066eeac9220e63f37223edc2035af7e2
bitmavrick <email.mehedi.bd@gmail.com>
2026-03-10 10:48:39 +0600

    Update QuickStartButton to use rounded power icon

--- feature/shortcuts/src/main/java/com/bitmavrick/shortcuts/components/QuickStartButton.kt
+++ feature/shortcuts/src/main/java/com/bitmavrick/shortcuts/components/QuickStartButton.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PowerSettingsNew
+import androidx.compose.material.icons.rounded.PowerSettingsNew
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -61,7 +61,7 @@ fun QuickStartButton(
                     modifier = Modifier.fillMaxSize()
                 ) {
                     Icon(
-                        imageVector = Icons.Filled.PowerSettingsNew,
+                        imageVector = Icons.Rounded.PowerSettingsNew,
                         contentDescription = null,
                         modifier = Modifier.size(62.dp)
                     )
